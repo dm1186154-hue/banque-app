82f2f9610ecee2df834b9d74121c9c3ab776ea85
dm1186154-hue <dm1186154@gmail.com>
2025-10-27 00:13:04 +0000

    Update firebase.js
    Signed-off-by: dm1186154-hue <dm1186154@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,21 +1,19 @@
-<!-- Firebase App (base SDK) -->
-<script src="https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js"></script>
+// Importation des fonctions Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
-<!-- Firebase Auth -->
-<script src="https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js"></script>
+// Configuration de ton projet Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAwGbQuv9EoIvBm7WC0OLTQ41cgleXd804",
+  authDomain: "banque-app-66bf9.firebaseapp.com",
+  projectId: "banque-app-66bf9",
+  storageBucket: "banque-app-66bf9.appspot.com",
+  messagingSenderId: "833823730245",
+  appId: "1:833823730245:web:64ddd7dbf2d73d24f9912c"
+};
 
-<script>
-  // Configuration Firebase
-  const firebaseConfig = {
-    apiKey: "AIzaSyAwGbQuv9EoIvBm7WC0OLTQ41cgleXd804",
-    authDomain: "banque-app-66bf9.firebaseapp.com",
-    projectId: "banque-app-66bf9",
-    storageBucket: "banque-app-66bf9.appspot.com",
-    messagingSenderId: "61613833199",
-    appId: "1:61613833199:web:5d4c81a1d8ecb6f7f3d6e0"
-  };
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Initialiser Firebase
-  const app = firebase.initializeApp(firebaseConfig);
-  const auth = firebase.getAuth(app);
-</script>
+// Initialiser l’authentification
+export const auth = getAuth(app);
