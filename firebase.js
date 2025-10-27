8b770343aa9a04a0066d52ec3a807a11d9f67b6c
dm1186154-hue <dm1186154@gmail.com>
2025-10-27 00:39:30 +0000

    Update firebase.js
    Signed-off-by: dm1186154-hue <dm1186154@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
-// Importation des fonctions Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
 
-// Configuration de ton projet Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAwGbQuv9EoIvBm7WC0OLTQ41cgleXd804",
   authDomain: "banque-app-66bf9.firebaseapp.com",
   projectId: "banque-app-66bf9",
   storageBucket: "banque-app-66bf9.appspot.com",
-  messagingSenderId: "833823730245",
-  appId: "1:833823730245:web:64ddd7dbf2d73d24f9912c"
+  messagingSenderId: "606307063540",
+  appId: "1:606307063540:web:b5c4a0ef0b07b962f7dd67"
 };
 
-// Initialiser Firebase
+// Initialisation Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialiser l’authentification
 export const auth = getAuth(app);
